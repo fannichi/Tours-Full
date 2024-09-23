04f3b8d3e7f1a73aebff47600d4513794e7655c1
yousseffannichi <youssef.fannichi001@umb.edu>
2024-09-23 15:44:54 -0400

    implemented cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const cors = require('cors');
 const AppError = require('./utils/appError');
@@ -30,6 +31,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Global MiddleWare
+// implement cors
+app.use(cors());
+// Access-Control-Allow-Origin *
+
+app.options('*', cors());
 
 // Security http headers
 app.use(helmet());
